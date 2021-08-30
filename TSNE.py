1f96c8f1b18516faf4db007912836c2f8c3cd588
Ahmed Salman <ge59dab@tum.de>
2021-08-30 20:48:24 +0200

    TSNE.py updated with scatterplot for any two classes

--- TSNE.py
+++ TSNE.py
@@ -10,36 +10,73 @@ import pandas as pd
 
 
 sns.set(rc={'figure.figsize':(11.7,8.27)})
-palette = sns.color_palette("tab20", 13)
+
 
 file = '/home/asalman/ifcwork/ifcworkspi2021/geometric-ifc/resources/feature_space.csv'
 file2 = '/home/asalman/ifcwork/ifcworkspi2021/geometric-ifc/resources/feature_space_withlabels.csv'
 reader = csv.reader(open(file, "rt"), delimiter=",")
 reader2 = csv.reader(open(file2, "rt"), delimiter=",")
-features = np.array(list(reader)).astype("float")
-labels = np.array(list(reader2)).astype("str")
 
 
-def Extract(label):
-    return [item[0] for item in label]
-      
-# Driver code
-
-labels = Extract(labels)
-
-
-
-
-X, y = load_digits(return_X_y=True)
-
+mode = "all"
+#mode = "two"
 
 tsne = TSNE()
 
-X_embedded = tsne.fit_transform(features)
+if mode == "two":
+    palette = sns.color_palette("tab20", 2)
 
-sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette)
+    features =[]
+    labels =[]
+    for row in reader2:
+        if row[0] == "IfcDistributionControlElement" or row[0] == "IfcFLowController":
+            labels.append(row[0])
+            row = row[1:]
+            features.append(row)
+       
+    X_embedded = tsne.fit_transform(features)
 
-#plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
+    sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette)
 
-plt.show()
+    #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
+
+    plt.show()
+
+if mode == "all":
+
+    palette = sns.color_palette("tab20", 13)
+
+    features = np.array(list(reader)).astype("float")
+    labels = np.array(list(reader2)).astype("str")
+
+    def Extract(label):
+        return [item[0] for item in label]
+
+    labels = Extract(labels)
+
+    X_embedded = tsne.fit_transform(features)
+
+    sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette)
+
+    #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
+
+    plt.show()
+    
+
+#def Extract(label):
+#    return [item[0] for item in label]
+      
+# Driver code
+
+#labels = Extract(labels)
+
+
+
+
+#X, y = load_digits(return_X_y=True)
+
+
+
+
+
 
