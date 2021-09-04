acfe42b69a8ca029010ffef31c399b4d1221b0ae
Ahmed Salman <ge59dab@tum.de>
2021-09-04 21:32:30 +0200

    TSNE code finalised

--- TSNE.py
+++ TSNE.py
@@ -12,247 +12,172 @@ import os
 
 
 
+## Chose the config below 
 
-
-sns.set_style("white")
-sns.set(rc={'figure.figsize':(11.7,8.27),'axes.facecolor':'white', 'figure.facecolor':'white'})
-
-tsne = TSNE()
 
 method = "KNN"
 #method = "Mesh"
 
+label = 'Real'
+#label = 'Predicted'
+
+mode = "Rotation Variant"
+#mode = "Rotation Invariant"
+
+numOfClasses  = 13
+#numOfClasses = 2
+#numOfClasses = 3
+
+subclasses = ['IfcFurnishingElement', 'IfcFlowSegment']
+
+def scatterplot(directory, numOfClasses, label):
+    
+
+    dir = directory
+    featureSpaceFile = os.path.join(dir, 'featurespace.csv')
+    predictionsFile = os.path.join(dir, 'trueVspred-withId.csv')
+
+    df = pd.read_csv(predictionsFile)
+    dict_map = {1: 'IfcFlowController', 2: 'IfcFlowFitting',3: 'IfcFlowSegment',
+        4: 'IfcFlowTerminal',5: 'IfcColumn',6: 'IfcFurnishingElement',7: 'IfcStair',8: 'IfcDoor',
+        9: 'IfcSlab',10: 'IfcWall',11: 'IfcWindow',12: 'IfcRailing',0: 'IfcDistributionControlElement'}
+    df.y_pred = [dict_map[item] for item in df.y_pred]
+    df.y_real = [dict_map[item] for item in df.y_real]
+   
+    print(featureSpaceFile)
+
+    featureSpace = csv.reader(open(featureSpaceFile, "rt"), delimiter=",")
+   
+    
+    features = []
+    labels = []
+    label = label
+    print(label)
+            
+    next(featureSpace)
+    
+
+    if numOfClasses == 13:
+
+        for row in featureSpace:
+            
+            row = row[1:]
+            features.append(row)
+                 
+            
+        if label == 'Real':
+            for row in df.y_real:
+                
+                labels.append(row)
+                
+        else:
+            for row in df.y_pred:
+                labels.append(row)
+
+    if numOfClasses == 2:
+
+        for row in featureSpace:
+            if row[0] == subclasses[0] or row[0] == subclasses[1]:
+                labels.append(row[0])
+                row = row[1:]
+                features.append(row)
+
+    if numOfClasses == 3:
+        
+        while len(subclasses) == 3:
+            for row in featureSpace:
+                if row[0] == subclasses[0] or row[0] == subclasses[1] or row[0] == subclasses[2]:
+                    labels.append(row[0])
+                    row = row[1:]
+                    features.append(row)
+        else:
+            print('Error: Enter at least 3 subclasses')
+            exit()
+            
+        
+        
+
+
+    tsne = TSNE()
+    X_embedded = tsne.fit_transform(features)
+    x = X_embedded[:,0]
+    y = X_embedded[:,1]
+
+    fig, ax = plt.subplots()
+    labels, index = np.unique(labels, return_inverse=True)
+    
+    sc = ax.scatter(x, y, marker = '.', c = index, alpha = 1)
+    ax.legend(sc.legend_elements()[0], labels)
+
+    plt.xlim(x.min()*0.95, x.max()*1.05)
+    plt.ylim(y.min()*0.95, y.max()*1.05)
+            
+    annot = ax.annotate("", xy=(0,0), xytext=(5,5),textcoords="offset points")
+    annot.set_visible(False)
+
+    def hover(event):
+
+        # check if event was in the axis
+        if event.inaxes == ax:
+            
+            cont, ind = sc.contains(event)
+            if cont:
+                # change annotation position
+                annot.xy = (event.xdata, event.ydata)
+                # write the name of every point contained in the event
+                annot.set_text("{}".format(', '.join([str(n) for n in ind["ind"]])))
+                annot.set_visible(True)    
+            else:
+                annot.set_visible(False)
+        
+
+        plt.draw()
+            
+                      
+    fig.canvas.mpl_connect("motion_notify_event", hover)
+    plt.title(method + ': ' + mode)
+    plt.show()  
+
+
+
+
+tsne = TSNE()
+
+
+
 if method == "KNN":
 
-    file1 = os.getcwd()
-    file1 = os.path.join(file1, "resources/knn_rot2.csv")
-    file2 = os.getcwd()
-    file2 = os.path.join(file2, "resources/knn_rot3.csv")
+    current_dir = os.getcwd()
+    file1 = os.path.join(current_dir, "resources/knn_easy")
+    file2 = os.path.join(current_dir, "resources/knn_full")
+
+else:
+
+    current_dir = os.getcwd()
+    file1 = os.path.join(current_dir, "resources/mesh_easy")
+    file2 = os.path.join(current_dir, "resources/mesh_full")
    
-    reader1 = csv.reader(open(file1, "rt"), delimiter=",")
-    reader2 = csv.reader(open(file2, "rt"), delimiter=",")
-    
+   
 
-    
-    #mode = "allwithrotationvariant"
-    mode = "twowithrotationvariant"
-    #mode = "allwithrotationinvariant"
-    #mode = "twowithrotationinvariant"
+if mode == "Rotation Variant":
+    if label == 'Prediction' and numOfClasses != 13: 
+        print('Error: Prediction cofiguration can only be used with all classes')
 
-
-    if mode == "allwithrotationvariant":
-
+    else:
+        scatterplot(file1,numOfClasses,label)
         
-        palette = sns.color_palette("tab20", 13)
-
-        features = []
-        labels = []
-
-        for row in reader1:
-            labels.append(row[0])
-            row = row[1:]
-            features.append(row)
-
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='kNN: Rotation Variant')
-
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        plt.show()
-
-    if mode == "allwithrotationinvariant":
-
-        palette = sns.color_palette("tab20", 13)
-
-        features = []
-        labels = []
-
-        for row in reader2:
-            labels.append(row[0])
-            row = row[1:]
-            features.append(row)
-
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='kNN: Rotation Invariant')
-
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        plt.show()
-    if mode == "twowithrotationvariant":
-
-        
-        
-        
-        
-
-
-
-        palette = sns.color_palette("bright", 3)
-
-        features =[]
-        labels =[]
-        for row in reader1:
-            if row[0] == "IfcFurnishingElement" or row[0] == "IfcFlowSegment" or row[0] == "IfcFlowTerminal":
-                labels.append(row[0])
-                row = row[1:]
-                features.append(row)
-
-        X_embedded = tsne.fit_transform(features)
-
-        fig, ax = plt.subplots()
-        x = X_embedded[:,0]
-        y = X_embedded[:,1]
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette)
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-        lnx = plt.plot([60,60], [0,1.5], color='black', linewidth=0.3)
-        lny = plt.plot([0,100], [1.5,1.5], color='black', linewidth=0.3)
-        lnx[0].set_linestyle('None')
-        lny[0].set_linestyle('None')
-        plt.xlim(x.min()*0.95, x.max()*1.05)
-        plt.ylim(y.min()*0.95, y.max()*1.05)
-        
-        def hover(event):
-            
-            lnx[0].set_data([event.xdata, event.xdata], [0, 1.5])
-            lnx[0].set_linestyle('--')
-            lny[0].set_data([0,100], [event.ydata, event.ydata])
-            lny[0].set_linestyle('--')
-        
-        fig.canvas.mpl_connect("motion_notify_event", hover)
-
-        plt.title('kNN: Rotation Variant')
-
-        plt.show()
-
-    if mode == "twowithrotationinvariant":
-        palette = sns.color_palette("bright", 3)
-
-        features =[]
-        labels =[]
-        for row in reader2:
-            if row[0] == "IfcFurnishingElement" or row[0] == "IfcFlowSegment" or row[0] == "IfcFlowTerminal":
-                labels.append(row[0])
-                row = row[1:]
-                features.append(row)
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='kNN: Rotation Invariant')
-
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        plt.show()
-
-if method == "Mesh":
-
-    file1 = os.getcwd()
-    file1 = os.path.join(file1, "resources/mesh_rot2.csv")
-    file2 = os.getcwd()
-    file2 = os.path.join(file2, "resources/mesh_rot3.csv")
-    
-    reader1 = csv.reader(open(file1, "rt"), delimiter=",")
-    reader2 = csv.reader(open(file2, "rt"), delimiter=",")
-
-    method = "KNN"
-    method = "Mesh"
-    #mode = "allwithrotationvariant"
-    #mode = "twowithrotationvariant"
-    #mode = "allwithrotationinvariant"
-    #mode = "twowithrotationinvariant"
-
-
-    if mode == "allwithrotationvariant":
-
-        palette = sns.color_palette("tab20", 13)
-
-        features = []
-        labels = []
-
-        for row in reader1:
-            labels.append(row[0])
-            row = row[1:]
-            features.append(row)
-
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='Mesh: Rotation Variant')
-
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        plt.show()
-
-    if mode == "allwithrotationinvariant":
-
-        palette = sns.color_palette("tab20", 13)
-
-        features = []
-        labels = []
-
-        for row in reader2:
-            labels.append(row[0])
-            row = row[1:]
-            features.append(row)
-
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='Mesh: Rotation Invariant')
-
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        plt.show()
-    if mode == "twowithrotationvariant":
-        palette = sns.color_palette("bright", 2)
-
-        features =[]
-        labels =[]
-        for row in reader1:
-            if row[0] == "IfcSlab" or row[0] == "IfcDoor":
-                labels.append(row[0])
-                row = row[1:]
-                features.append(row)
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='Mesh: Rotation Variant')
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        fig, ax = plt.subplots()
-        #ax.scatter(X_embedded[:,0], X_embedded[:,1], c=labels.map(palette))
-        plt.show
-
-
-    if mode == "twowithrotationinvariant":
-        palette = sns.color_palette("bright", 2)
-
-        features =[]
-        labels =[]
-        for row in reader2:
-            if row[0] == "IfcSlab" or row[0] == "IfcDoor":
-                labels.append(row[0])
-                row = row[1:]
-                features.append(row)
-
-        X_embedded = tsne.fit_transform(features)
-
-        sns.scatterplot(x=X_embedded[:,0], y=X_embedded[:,1], hue=labels,legend='full', palette = palette).set(title='Mesh: Rotation Invariant')
-
-
-        #plt.scatter(X_embedded[:,0],y=X_embedded[:,1])
-
-        plt.show()
 
     
+    
+if mode == "Rotation Invariant":
+
+    if label == 'Prediction' and numOfClasses != 13: 
+        print('Error: Prediction cofiguration can only be used with all classes')
+
+    else:
+        scatterplot(file2,numOfClasses,label)
+        
+
+
+        
 
